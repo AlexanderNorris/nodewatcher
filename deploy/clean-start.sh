2b09654843fcd177766adc0816d8378133ab7777
Alexander Norris <Alex.Norris@live.co.uk>
2023-07-14 15:32:16 +0000

    Remove built images on clean start

--- deploy/clean-start.sh
+++ deploy/clean-start.sh
@@ -1,4 +1,5 @@
 docker compose down
 docker rm -f $(docker ps -a -q)
 docker volume rm $(docker volume ls -q)
+docker image rm $(docker image ls | grep nodewatcher | awk {'print $3'})
 docker compose up
